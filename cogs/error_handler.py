44d19ef4988e1360ef164e87d2b1faa043337882
Ethanol <ethanolchennault@gmail.com>
2020-11-26 01:18:42 -0800

    added throwing of error

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -28,7 +28,7 @@ class CommandErrorHandler(commands.Cog, command_attrs=dict(hidden=True)):
             responses = get_responses()[command.cog.qualified_name.lower()][command.name]
             embed = embed_template(title="An internal responder message error has occured", description=f"Missing responder `{error.args[0]}` for command `command.name`")
             embed.color = 15138816
-            return await ctx.send(content=None, embed=embed)
+            await ctx.send(content=None, embed=embed)
         else:
             try:
                 command = self.client.get_command(ctx.invoked_with)
@@ -49,10 +49,12 @@ class CommandErrorHandler(commands.Cog, command_attrs=dict(hidden=True)):
                 
                 embed = embed_template(title="An error has occured", description=message)
                 embed.color = 15138816
-                return await ctx.send(content=None, embed=embed)
+                await ctx.send(content=None, embed=embed)
             except KeyError:
                 command = self.client.get_command(ctx.invoked_with)
                 responses = get_responses()[command.cog.qualified_name.lower()][command.name]
                 embed = embed_template(title="An internal responder message error has occured", description=f"Missing responder `{error.args[0]}` for command `{command.name}`")
                 embed.color = 15138816
-                return await ctx.send(content=None, embed=embed)
+                await ctx.send(content=None, embed=embed)
+
+        raise error
